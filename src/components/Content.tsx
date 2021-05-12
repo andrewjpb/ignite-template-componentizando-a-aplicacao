64ea20580fc3ecf145f33fc799b5651f269b3ba9
Andrew Pereira <andrew.jpb@outlook.com>
2021-05-12 15:44:01 -0300

    Separations of components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import "../styles/content.scss";
+import { GenreResponseProps, MovieProps } from "../App";
+
+type propsDTO = {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+};
+
+export function Content({ selectedGenre, movies }: propsDTO) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie: any) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
